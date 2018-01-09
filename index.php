4955b09d251321f4c5fa9c6b434ce75cb92f62f9
Giperion-x <35272758+Giperion-x@users.noreply.github.com>
2018-01-09 23:38:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ echo "<!DOCTYPE html>
 <meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\">
 <title>Open Server</title>
 </head>
-<body style=\"background: url(ea.jpg) top left repeat-x\">
+<body style=\"background: url(https://github.com/Giperion-x/giperion.github.io/blob/master/ea.jpg) top left repeat-x\">
 <center>
 <br><br><br><br><br><br><br><br><br><br><br><div style=\"width: 600px;\"><span style=\"font-size: 40px; color: #FFFF99; font-family: Arial, Verdana; text-shadow: 2px 3px 2px black\">  * Hello World ! *</span>
 <br><br><br><br><br><span style=\"font-size: 32px; color: yellow; font-family: Verdana,Arial;text-shadow: 2px 3px 2px black\">Он работает,ураaa :)</span>
